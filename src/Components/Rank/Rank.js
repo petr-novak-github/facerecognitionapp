7bdcd6b674f27e8d6d40c7f77e2c7469a19b2a96
Petr D. Novak <petr-novak-donald@protonmail.com>
2020-07-28 10:42:10 +0200

    Add all done files in section 252 (course ZtoM on Udemy.com : Web Developer Zero to Mastery in 2020 .. or smthing like that)

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,19 @@
+import React from 'react';
+/*import './ImageLinkForm.css' */
+
+
+const Rank = () => {
+    return (
+        <div>
+          <div className='white f3'>
+            {'Petr, your current rank is...'}
+          </div>
+          <div className='white f1'>
+            {'5#'}
+          </div>
+        </div>
+    );
+
+}
+
+export default Rank;
